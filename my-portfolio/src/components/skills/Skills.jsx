cddbccca937aae435e0ae3c247b457a90745e924
Mamosebetsi Kekae <mkekae@gmail.com>
2022-05-28 16:35:03 -0700

    done with skills component and styling

--- my-portfolio/src/components/skills/Skills.jsx
+++ my-portfolio/src/components/skills/Skills.jsx
@@ -9,12 +9,87 @@ const Skills = () => {
 				<div className="experience__frontend">
 					<h3>FrontEnd Development</h3>
 					<div className="experience__content">
+						{/* fornt-end */}
+						{/* skill-1 */}
 						<article className="experience__details">
-							<BsBookmarkCheckFill />
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>HTML</h4>
+								<small className="text-align">Experienced</small>
+							</div>
+						</article>
+
+						{/* skill-2 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>CSS / Bootstrap</h4>
+								<small className="text-align">Experienced</small>
+							</div>
+						</article>
+
+						{/* skill-3 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>JavaScript</h4>
+								<small className="text-align">Intermediate</small>
+							</div>
+						</article>
+
+						{/* skill-4 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>React</h4>
+								<small className="text-align">Intermediate</small>
+							</div>
 						</article>
 					</div>
 				</div>
-				<div className="experience__frontend"></div>
+				{/* end of front-end*/}
+
+				<div className="experience__frontend">
+					<h3>BackEnd Development</h3>
+					<div className="experience__content">
+						{/* back-end */}
+						{/* skill-1 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>Solidity</h4>
+								<small className="text-align">Intermediate</small>
+							</div>
+						</article>
+
+						{/* skill-2 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>Truffle / Hardhat</h4>
+								<small className="text-align">Experienced</small>
+							</div>
+						</article>
+
+						{/* skill-3 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>OpenZeppelin /Geth </h4>
+								<small className="text-align">Experienced</small>
+							</div>
+						</article>
+
+						{/* skill-4 */}
+						<article className="experience__details">
+							<BsBookmarkCheckFill className="experience__details-icon" />
+							<div>
+								<h4>Metamask</h4>
+								<small className="text-align">Experienced</small>
+							</div>
+						</article>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
