7a1e23d06ca3ba8239af7542feae1058a5da0f9c
Mamosebetsi Kekae <mkekae@gmail.com>
2022-05-30 15:47:24 -0700

    finished portfolio component with its styling

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,104 @@
-
-import './portfolio.css'
+import './portfolio.css';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 const Portfolio = () => {
-  return (
+	return (
 		<section id="portfolio">
-			<div>Portfolio</div>
+			<h2>My Services</h2>.{' '}
+			<div className=" container services__container">
+				{/*blockchain-dev*/}
+				<article className="service">
+					<div className=" service__head">
+						<h3>Blockchain Solidity Development</h3>
+					</div>
+					<ul className="sevice__list">
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+					</ul>
+				</article>
+				{/*blockchain-dev end*/}
+
+				{/*SEO beggin*/}
+				<article className="service">
+					<div className=" service__head">
+						<h3>SEO Optimization</h3>
+					</div>
+					<ul className="sevice__list">
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+					</ul>
+				</article>
+				{/*SEO end*/}
+
+				{/*React beggin*/}
+				<article className="service">
+					<div className=" service__head">
+						<h3>React/Next JS Development</h3>
+					</div>
+					<ul className="sevice__list">
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+
+						<li>
+							<AiOutlineCheckCircle className="service__list-icon" />
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+					</ul>
+				</article>
+				{/*React end*/}
+			</div>
 		</section>
 	);
-}
+};
 
-export default Portfolio
+export default Portfolio;
